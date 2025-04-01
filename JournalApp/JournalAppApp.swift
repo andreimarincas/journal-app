d245d0be57aaf4a4280095ddfd445d5552d525f8
Andrei Marincas <andrei.a.marincas@gmail.com>
2025-04-01 12:52:13 +0300

    Initial Commit

---
+++ JournalApp/JournalAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by Andrei Marincas on 01.04.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct JournalAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ JournalAppTests/JournalAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppTests.swift
+//  JournalAppTests
+//
+//  Created by Andrei Marincas on 01.04.2025.
+//
+
+import Testing
+@testable import JournalApp
+
+struct JournalAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
